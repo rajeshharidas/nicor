16eeb6eebe0603df7e70df83082819f12b9be085
RAJESH HARIDAS <rharidas18@gmail.com>
2021-12-15 11:16:57 -0600

    Add files via upload

---
+++ src/main/java/com/rh/ai/utilities/nicor/NicorApplication.java
@@ -0,0 +1,13 @@
+package com.rh.ai.utilities.nicor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rh/ai/utilities/nicor/NicorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.ai.utilities.nicor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
